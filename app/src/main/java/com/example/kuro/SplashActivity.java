c3864c390440336b9eb740cd9bd246ebd24c9603
parth <parthnvaswani@gmail.com>
2022-10-26 00:36:10 +0530

    Added splash screen

---
+++ app/src/main/java/com/example/kuro/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.kuro;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }, 1000);
+    }
+}
